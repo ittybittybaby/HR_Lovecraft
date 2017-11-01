e4bf89bfeff77edab985b0dd7228b87b94fbb623
Basgolfer <Basgolfer@gmail.com>
2017-11-01 13:31:38 -0400

    Contact, contactTest, person, personTest completed

--- hrapp/src/main/java/hrlovecraft/Contact.java
+++ hrapp/src/main/java/hrlovecraft/Contact.java
@@ -3,8 +3,85 @@ package hrlovecraft;
 public class Contact {
     private String streetAddress;
     private String city;
-    private State enum;
+    private State state;
     private int phoneNumber;
-    private String emergencyConcatName;
+    private String emergencyContactName;
+    private int emergencyContactPhoneNumber;
+    private String email;
 
+    public Contact() {
+        this.streetAddress = null;
+        this.city = null;
+        this.state = null;
+        this.phoneNumber = 0;
+        this.emergencyContactName = null;
+        this.emergencyContactPhoneNumber = 0;
+        this.email = null;
+    }
+
+    public Contact(String streetAddress, String city, State state, int phoneNumber, String emergencyContactName, int emergencyContactPhoneNumber, String email) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.emergencyContactName = emergencyContactName;
+        this.emergencyContactPhoneNumber = emergencyContactPhoneNumber;
+        this.email = email;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmergencyContactName() {
+        return emergencyContactName;
+    }
+
+    public void setEmergencyContactName(String emergencyContactName) {
+        this.emergencyContactName = emergencyContactName;
+    }
+
+    public int getEmergencyContactPhoneNumber() {
+        return emergencyContactPhoneNumber;
+    }
+
+    public void setEmergencyContactPhoneNumber(int emergencyContactPhoneNumber) {
+        this.emergencyContactPhoneNumber = emergencyContactPhoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- hrapp/src/main/java/hrlovecraft/Person.java
+++ hrapp/src/main/java/hrlovecraft/Person.java
@@ -1,5 +1,32 @@
 package hrlovecraft;
 
 public class Person {
+    private String name;
+    private Contact contactInfo;
 
+    public Person() {
+        this.name = null;
+        this.contactInfo = null;
+    }
+
+    public Person(String name, Contact contactInfo) {
+        this.name = name;
+        this.contactInfo = contactInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Contact getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(Contact contactInfo) {
+        this.contactInfo = contactInfo;
+    }
 }

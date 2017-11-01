8e9a164e7fd1ba9d0dfa21c6d90af6fb44697984
logan <loganhanner@gmail.com>
2017-11-01 15:43:52 -0400

    added correct files

---
+++ hrapp/src/main/java/hrlovecraft/Benefits.java
@@ -0,0 +1,64 @@
+package hrlovecraft;
+
+public class Benefits {
+    private int sickDays;
+    private int vacationDays;
+    private boolean hasHealthcare;
+    private boolean hasRetirementPlan;
+
+
+    public Benefits() {
+        this.sickDays = 7;
+        this.vacationDays = 7;
+        this.hasHealthcare = false;
+        this.hasRetirementPlan = false;
+    }
+
+    public Benefits(int sickdays, int vacationDays, boolean hasHealthCare, boolean hasRetirementPlan) {
+        this.sickDays = sickdays;
+        this.vacationDays = vacationDays;
+        this.hasHealthcare = hasHealthCare;
+        this.hasRetirementPlan = hasRetirementPlan;
+    }
+
+    public int getSickDays() {
+        return sickDays;
+    }
+
+    public int getVacationDays() {
+        return vacationDays;
+    }
+
+    public boolean getHasHealthcare() {
+        return hasHealthcare;
+    }
+
+    public boolean getHasRetirementPlan() {
+        return hasRetirementPlan;
+    }
+
+    public void setSickDays(int number) {
+        this.sickDays = number;
+    }
+
+    public void setVacationDays(int number) {
+        this.vacationDays = number;
+    }
+//
+//    public void setBenefitPackageBySalaryTier(enum salaryTier) {
+//        switch (salaryTier) {
+//            case "low":
+//                hasHealthcare = false;
+//                hasRetirementPlan = false;
+//                break;
+//            case "mid":
+//                hasHealthcare = true;
+//                hasRetirementPlan = false;
+//                break;
+//            case "high":
+//                hasHealthcare = true;
+//                hasRetirementPlan = true;
+//                break;
+//        }
+//    }
+}
---
+++ hrapp/src/test/java/hrlovecraft/BenefitsTest.java
@@ -0,0 +1,50 @@
+package hrlovecraft;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BenefitsTest {
+
+    @Test
+    public void getSetSickDaysTest(){
+        Benefits benefits = new Benefits();
+        int sickDaysExpected = 25;
+        benefits.setSickDays(sickDaysExpected);
+        int sickDaysActual = benefits.getSickDays();
+        Assert.assertEquals(sickDaysExpected, sickDaysActual);
+    }
+
+    @Test
+    public void getSetVacationDays(){
+        Benefits benefits = new Benefits();
+        int vacationDaysexpected = 30;
+        benefits.setVacationDays(vacationDaysexpected);
+        int vacationDaysactual = benefits.getVacationDays();
+        Assert.assertEquals(vacationDaysexpected, vacationDaysactual);
+    }
+
+//    @Test
+//    public void getHasHealthcare(){
+//        Benefits benefits = new Benefits();
+//        boolean expected = false;
+//        benefits.setBenefitPackageBySalaryTier();
+//        boolean actual = benefits.getHasHealthcare();
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void getHasRetirementPlan(){
+//        Benefits benefits = new Benefits();
+//        boolean expected = false;
+//       benefits.setBenefitPackageBySalaryTier();
+//        boolean actual = benefits.getHasRetirementPlan();
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void setBenefitsPackageBySalaryTier(){
+//        Benefits benefits = new Benefits();
+//
+//    }
+
+}

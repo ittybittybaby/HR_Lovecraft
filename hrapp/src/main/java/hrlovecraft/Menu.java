0c8b4ebc3c6d583b41b79bf6b2496eca54c166ae
rob <rob@zipcoders-MacBook-Pro.local>
2017-11-02 15:30:27 -0400

    new employee and update current employee menus

--- hrapp/src/main/java/hrlovecraft/Menu.java
+++ hrapp/src/main/java/hrlovecraft/Menu.java
@@ -14,17 +14,97 @@ public class Menu {
 
 
     public void printWelcomeMessage() {
-        System.out.println("Welcome to the HR application\n"
-                            + "What would you like to do today\n"
-                            + "1) Hire New Employee\n"
-                            + "2) Manage Current Employees");
+        System.out.println("*****************************\n" +
+                           "Welcome to the HR application\n" +
+                           "*****************************\n\n" +
+                           "What would you like to do today\n" +
+                           "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                           "1) Hire New Employee\n" +
+                           "2) Manage Current Employees");
         Console.getConsole().handleWelcomeMessageInput();
     }
 
-    public void hireNewEmployee() {
+    public void hireNewEmployeeMenu() {
         System.out.println("Hire New Employee\n"
                          + "Please fill out the following information\n");
     }
+
+    public void setEmployeeName(){
+        System.out.println("Name: ");
+    }
+
+    public void setEmployeeStAddress(){
+        System.out.println("Street Address(House number and st. name): ");
+    }
+
+    public void setEmployeeCity(){
+        System.out.println("City: ");
+    }
+
+    public void setEmployeeState(){
+        System.out.println("State: ");
+    }
+
+    public void setEmployeePhoneNumber(){
+        System.out.println("Phone: ");
+    }
+
+    public void setEmployeeEmail(){
+        System.out.println("Email: ");
+    }
+
+    public void setEmployeeDepartment(){
+        System.out.println("Department: ");
+    }
+
+    public void setEmployeeStatus(){
+        System.out.println("Part time or Full time(PT/FT): ");
+    }
+
+    public void setEmployeeHireDate(){
+        System.out.println("Hire Date: ");
+    }
+
+    public void setEmployeeSalaryTier(){
+        System.out.println("Salary Tier: ");
+    }
+
+    public void setEmployeePosition(){
+        System.out.println("Position: ");
+    }
+
+    public void chooseEmployeeToManage(){
+        System.out.println("Enter Employee ID to update: ");
+    }
+
+    public void manageEmployeeMenu(){
+        System.out.println("What would you like to update?\n" +
+                           "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                           "1)Personal Information\n" +
+                           "2)Department\n" +
+                           "3)Status(FT/PT)\n" +
+                           "4)Termination Date\n" +
+                           "5)Salary Tier\n" +
+                           "6)Benefits\n" +
+                           "7)Position");
+    }
+
+    public void updatePersonalInfo(){
+        System.out.println("What would you like to update?\n" +
+                           "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                           "1)Name\n" +
+                           "2)Address\n" +
+                           "3)Phone Number\n" +
+                           "4)Email");
+    }
+
+    public void setEmployeeTerminationDate(){
+        System.out.println("Termination Date: ");
+    }
+
+//    public void setEmployeeBenefits(){
+//        System.out.println("Would you like benefits?");
+//    }
 
     public void manageCurrentEmployees() {
         System.out.println("Manage Current Employees\n"

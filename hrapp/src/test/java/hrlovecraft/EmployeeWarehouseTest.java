7295d86d5e1b26d8a9691bbde85c313f82019215
elliott <elliott@zipcoders-MacBook-Pro.local>
2017-11-03 13:09:53 -0400

    empUpdate

--- hrapp/src/test/java/hrlovecraft/EmployeeWarehouseTest.java
+++ hrapp/src/test/java/hrlovecraft/EmployeeWarehouseTest.java
@@ -1,16 +1,23 @@
 package hrlovecraft;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class EmployeeWarehouseTest {
 
-    EmployeeWarehouse eWH= EmployeeWarehouse.getInstance();
+
+    EmployeeWarehouse eWH = EmployeeWarehouse.getInstance();
+    {
+        for (int i = 1; i < 10; i++)
+            eWH.add("bob", "123 fake st", "wilminton", "DELAWARE", "5559991234", "none@zip.com");
+    }
     @Test
     public void addTest() throws Exception {
         Employee expected=new Employee(44,"tom","123 st", "newark", "delaware", "2222222222", "zip@zippity.com");
+
         eWH.add(expected);
         Employee actual =eWH.get(44);
 
